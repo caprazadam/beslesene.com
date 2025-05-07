fef76651c342c86b82ed8ac208f73dd13449c2d8
Ömer Damoğlu <caprazadam@gmail.com>
2025-05-07 14:55:38 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ const inter = Inter({ subsets: ["latin"] })
 export const metadata: Metadata = {
   title: "Game App",
   description: "Mobile game application",
-    generator: 'v0.dev'
+    generator: 'beslesene'
 }
 
 export default function RootLayout({
